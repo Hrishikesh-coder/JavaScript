0fdd62f029206774fc971ebf909de940da0921cb
Hrishikesh-coder <hrishipotter123@gmail.com>
2020-11-14 14:32:58 +0530

    added some files

---
+++ ternary.js
@@ -0,0 +1,11 @@
+var authenticated = true;
+
+// if(authenticated){
+
+//     console.log("Show sign out!");
+
+// }
+
+// else{console.log("SHow Log in !");}
+
+authenticated ? console.log("Singout") : console.log("SIgnin"); 
