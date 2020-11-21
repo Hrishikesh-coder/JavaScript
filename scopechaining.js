896335e4b082c94e4d3f0410b6eba027e0fe16f2
Hrishikesh-coder <hrishipotter123@gmail.com>
2020-11-21 18:20:27 +0530

    new

--- scopechaining.js
+++ scopechaining.js
@@ -1,3 +1,15 @@
 var name = "hrishi";
 
 console.log(`name : ${name}`);
+
+function new4(){
+    //var name = "h";
+    console.log(name);
+    new2();
+    function new2(){
+        //var name="hr";
+        console.log(name);
+    }
+}
+
+new4();
