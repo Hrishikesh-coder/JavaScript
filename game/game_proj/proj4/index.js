d537f0394d5f5bdbb219848e54b59396cafa7b30
Hrishikesh-coder <hrishipotter123@gmail.com>
2020-12-02 13:46:53 +0530

    did the game

--- game/game_proj/proj4/index.js
+++ game/game_proj/proj4/index.js
@@ -1,17 +1,66 @@
-const cards = document.querySelectorAll(".card")
+const cards = document.querySelectorAll(".card");
+console.log(cards);
 
-var flip = () => {
+var isFlipped = false;
+var firstCard, secondCard;
+
+cards.forEach( (card) => card.addEventListener("click",flip))
+
+function flip(){
+
+    // console.log("Card Flipped");
+
+    this.classList.add("flip");
+    if(!isFlipped){
+        isFlipped = true;
+        firstCard = this;
+
+    }
+    else{
+        secondCard = this;
+        console.log(firstCard);
+        console.log(secondCard);
+        check();
+
+    }
+}
+
+function check(){
+    // console.log("Checking....");
+    if(firstCard.dataset.image === secondCard.dataset.image){success();}
+    else{fail();}
+}
+
+function success(){
+
+    console.log("Success");
+    firstCard.removeEventListener('click',flip);
+    secondCard.removeEventListener('click',flip);
+    reset()
+}
+
+function fail() {
+
+    setTimeout(()=>{
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove("flip");
+    },1000);
 
 }
 
-var success = () => {
+function reset() {
+
+    isFlipped = false;
+    firstCard = null;
+    secondCard = null;
 
 }
 
-var fail = () => {
+(function shuffle(){
 
-}
+    cards.forEach((card) => {
+        var index = Math.floor(Math.random() * 16);
+        card.style.order = index;
+    })
 
-var reset = () => {
-
-}
+})();
