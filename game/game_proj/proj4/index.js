f30de7ac700fc80e92ba61980242a93d426b3167
Hrishikesh-coder <hrishipotter123@gmail.com>
2021-04-06 14:44:10 +0530

    added some new code

--- game/game_proj/proj4/index.js
+++ game/game_proj/proj4/index.js
@@ -81,4 +81,6 @@ button.addEventListener("click",() => {
     cards.forEach( (card) => card.addEventListener("click",flip))
 
 })
-
+ //34f1f7ce2a897812104287ffc47eeaf1
+ //https://api.themoviedb.org/3/movie/550?api_key=34f1f7ce2a897812104287ffc47eeaf1
+ //eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNGYxZjdjZTJhODk3ODEyMTA0Mjg3ZmZjNDdlZWFmMSIsInN1YiI6IjVmYzc1ZDM2ZmJhNjI1MDA0MmEzYWUxNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.WaBR8cPaaTqxSOiiksN-wIztB9y5MWXTjG6I0vJHIps
